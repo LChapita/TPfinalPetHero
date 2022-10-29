797b7322b8879916fb22f95e9713e1ac58e8b878
CarolinaBeatrizZoppiConeja <115192954+CarolinaBeatrizZoppiConeja@users.noreply.github.com>
2022-10-29 19:32:50 -0300

    formreservacaro.php

---
+++ formreserva.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>reservas</title></head>
+        <body>
+            <form action="" method="post">
+                <label for="elegir">hola <?php  Owner.getId(); $Owner->getName() ;$Owner->getsurName()?> <br>
+                elija los dias que necesita un keeper</label><br><br>
+                
+                <label for="" name="fechastart">date start</label>
+                <input type="text" name="fechastart" id="fechastart"><br><br>
+
+                <label for="" name="fechafinish">date finish</label>
+                <input type="text" name="fechafinish" id="fechafinish"><br><br>
+
+            </form>
+        </body>
+</html>
