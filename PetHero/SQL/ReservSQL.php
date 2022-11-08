399953f026aa7beb94153beb18aca3f409676570
elianolivera <72286803+elianolivera@users.noreply.github.com>
2022-11-07 23:33:55 -0300

    consult
    consult

---
+++ PetHero/SQL/ReservSQL.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace SQL;
+
+use SQL\IReservSQL as IReservSQL;
+use SQL\Connection as Connection;
+use \Exception as Exception;
+use Models\Reserv as Reserv;
+
+class ReservSQL implements IReservSQL{
+    private $connection;
+    private $tableName="reserv";
+
+    public function Add(Reserv $reserv)
+    {
+        try
+        {
+            $query = "CALL Reserv_Add(?, ?, ?, ?, ?)";
+
+            $parameters["idReserv"] = $reserv->getIdReserv();
+            $parameters["idOwner"] = $reserv->getIdOwner();
+            $parameters["idKeeper"] = $reserv->getIdKeeper();
+            $parameters["dateStart"] = $reserv->getDateStart();
+            $parameters["dateFinish"] = $reserv->getDateFinish();
+
+            $this->connection=Connection::GetInstance();
+
+            $this->connection->ExecuteNonQuery($query, $parameters, QueryType::StoredProcedure);
+
+            
+        }catch(Exception $ex){
+            throw $ex;
+        }
+    }
+    public function Confirm(Reserv $reserv)
+    {
+        try
+        {
+            $query = "CALL Reserv_Confirm(?)";
+
+            $parameters["confirm"] = $reserv->getConfirm();
+        
+
+            $this->connection=Connection::GetInstance();
+
+            $this->connection->ExecuteNonQuery($query, $parameters, QueryType::StoredProcedure);
+            
+        }catch(Exception $ex){
+            throw $ex;
+        }
+    }
+    
+    public function GetAll()
+    {
+        try{
+            $reservList = array();
+
+            $query = "SELECT * FROM " . $this->tableName;
+
+            $this->connection = Connection::GetInstance();
+
+            $resultSet = $this->connection->Execute($query);
+            foreach ($resultSet as $row)
+            {                
+                $reserv = new Reserv();
+                $reserv->setIdReserv($row["idReserv"]);
+                $reserv->setIdOwner($row["idOwner"]);
+                $reserv->setIdKeeper($row["idKeeper"]);
+                
+                $reserv->setDateStart($row["dateStart"]);
+                $reserv->setDateFinish($row["dateFinish"]);
+                
+                array_push($reservList, $reserv);
+                }
+                
+                return $reservList;
+         }
+        catch(Exception $ex)
+        {
+                throw $ex;
+        }
+    
+    }
+    public function GetOwnerbyId($id)
+    {
+        try{
+            $reservList = array();
+
+            $query = "SELECT * FROM " . $this->tableName ."WHERE idOwner=".$id;
+
+            $this->connection = Connection::GetInstance();
+
+            $resultSet = $this->connection->Execute($query);
+
+             foreach ($resultSet as $row)
+                {                
+                    $reserv = new Reserv();
+                    $reserv->setIdReserv($row["idReserv"]);
+                    $reserv->setIdReserv($row["idOwner"]);
+                    $reserv->setIdReserv($row["idKeeper"]);
+
+                    $reserv->setIdReserv($row["dateStart"]);
+                    $reserv->setIdReserv($row["dateFinish"]);
+
+                    array_push($reservList, $reserv);
+                }
+
+                return $reservList;
+        
+         }
+        catch(Exception $ex)
+        {
+                throw $ex;
+        }
+    
+    }
+}
---
+++ PetHero/Views/owners/list-reserv.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once(VIEWS_PATH . "validate-session.php");
+
+use DAO\KeeperDAO;
+use DAO\userDAO;
+use Models\User;
+use Models\Keeper;
+use Models\Owner;
+use Models\Reserv as Reserv;
+use DAO\ReservDAO;
+use SQL\ReservSQL;
+
+require_once(VIEWS_PATH."nav.php");
+
+?>
+<main class="py-5">
+    <section id="listado" class="mb-5">
+        <div class="container">
+            <h2 class="mb-4">Listado de Reservass</h2>
+            <table class="table bg-light text-center">
+                <thead class="bg-dark text-white">
+                    <th>Id Reserva</th>
+                    <th>Id Owner</th>
+                    <th>Id Keeper</th>
+                    <th>Date Start</th>
+                    <th>Date Finish</th>
+                    <th>Confirmed</th>
+                   
+                </thead>
+                <tbody>
+                    <?php
+
+                    //$ReservDAO=new ReservDAO();
+                    //$reserv = new Reserv();
+                    //$reservList = $ReservDAO->GetAll();
+                    $reservSQL=new ReservSQL();
+                    $reserv=new Reserv();
+                    $reservList=$reservSQL->GetAll();
+                    
+                    foreach ($reservList as $reserv) { 
+                    //var_dump($reserv);
+                    ?>
+                        <tr>
+                        <td><?php echo $reserv->getSetConfirmed(); ?></td>
+                            <td><?php echo $reserv->getIdReserv(); ?></td>
+                            <td><?php echo $reserv->getIdOwner(); ?></td>
+                            <td><?php echo $reserv->getIdKeeper(); ?></td>
+                            <td><?php echo $reserv->getDateStart(); ?></td>
+                            <td><?php echo $reserv->getDateFinish(); ?></td>
+                            <td><?php echo $reserv->getConfirmed(); ?></td>
+
+                        </tr>
+
+                    <?php
+
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </section>
+</main>
