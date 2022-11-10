25fd7e56005a423fd8d392b1ed40213c055a39d3
CarolinaBeatrizZoppiConeja <115192954+CarolinaBeatrizZoppiConeja@users.noreply.github.com>
2022-11-10 18:14:49 -0300

    cupon de pago mejorado

---
+++ cupon de pago mejorado.php
@@ -0,0 +1,135 @@
+<html>
+    <head>
+    <style>
+        .pe{
+            color:white;
+        }
+        .tabla {
+            font-size:15pt;
+            text-align:center;
+        }
+        td{
+            font-size: 9px;
+           
+        }
+        .t{
+            font-size:10px;
+        }
+         div {
+            border: 2px solid green;
+            width:350px; height:50px;
+            float:left;
+            margin:10px;
+        }
+          
+        strong{
+            font-size: 15px;
+        }  
+         
+            
+        h1{
+            background-color:green;
+            color : white;
+            text-align:right;
+            font-size: 20px;
+
+        }
+        h5{font-size: 18px;
+            background-color: brown;
+             color:antiquewhite}
+        span{
+            width: 175; height: 25;
+            float:left;
+        }
+        .spandex{
+            border:3px solid black;
+            width:350;height: 0px;
+        }
+        .spandex4{
+            margin: 0px; padding: 0px;
+            border: 1px solid black;
+            width:350;height: 0px;
+        
+        }
+        .spandex2{
+            border: 1px solid black;
+            width:0px;
+            height:40px;
+        }
+        .spandex3{
+            width: 360; height: 1000;
+        }
+        h2{
+            background-color:brown;
+            color : antiquewhite;
+            text-align:left;
+            font-size: 20px;
+        }
+        h3{
+            font-size: 13px;
+            text-align: left;
+            
+        }
+       
+        .h31{
+            font-size: 14px;
+            width:350;
+            text-align: right;
+            
+        }
+    
+    </style>
+    </head>
+    <body>
+        <span class="spandex3">
+        <span><h2>the pets</h2></span>
+        <span><h1>payment coupon</h1></span>
+        <br>
+        <br>
+        
+         <br><table><tr><td align:left><h3><strong>N° 00000566787</strong></h3></td> <td class="pe">gfcbidferfrgrtgrtgrtfiurhfiuerhfuhfcreuifheivfre</td><td align:right><h3> 00/00/0000</h3></td></tr></table>
+        <span class="spandex"></span>
+        <div>
+        <table>
+                <tr><td>1er vencimiento</td><td class="pe">hgytffuyjgjuhjhhhhhhh</td><td rowspan="3"><p class="pe">oo<span class="spandex2"></span>jujjhhb</p> </td><td> importe</td> </tr>
+                <tr><td><strong>00/00/0000</strong></td><td class="pe">jygyfffhfgygyg</td><td> <strong>$000000000</strong></td></tr>
+        </table>
+        </div>
+        <div>
+        <table>
+                <tr><td>2do vencimiento</td><td class="pe">hgytffuyjgjuhjhhhhhhh</td><td rowspan="3"><p class="pe">oo<span class="spandex2"></span>jujjhhb</p> </td><td> importe</td> </tr>
+                <tr><td><strong>00/00/0000</strong></td><td class="pe">jygyfffhfgygyg</td><td> <strong>$????????</strong></td></tr>
+        </table>
+        </div>
+        <span class="spandex"></span>
+        <table>
+             <tr><td colspan="3" class="t">datos del owner</td></tr>
+                <tr><td>name</td><td> -----------------</td> </tr>
+                <tr><td>surname</td> <td> -------------</td></tr>
+                <tr><td>DNI</td><td> 34533205234</td></tr></table>
+                <span class="spandex4"></span>
+                <table>
+                <tr><td colspan="3" class="t">datos de keeper</td></tr>
+                <tr><td>keeper</td><td>---------------</td></tr>
+                <tr><td>tuition</td><td> -------------------</td></tr>
+                </table>
+                <span class="spandex4"></span>
+                <table>
+                <tr><td colspan="3" class="t">datos of pets</td></tr>
+                <tr><td>idpets</td><td> --------------</td></tr>
+                <tr><td>description 's pets</td><td>-----------</td></tr>
+               <tr><td colspan="3" class="t">datos contables</td></tr>
+                </table>
+               <span class="spandex4"></span>
+               <table>
+                <tr><td>price per pet size</td><td>$---------</td></tr>
+                <tr><td>number of days</td><td> -------</td></tr>
+                <tr><td>date start</td><td> 00/00/0000</td></tr>
+                <tr><td>date finish</td><td>00/00/0000</td></tr>
+                 <tr><td>amount payable</td><td>$-------------</td></tr>
+                
+            </table>
+            <span class="spandex"></span>
+        </span>
+    </body>
+</html>
