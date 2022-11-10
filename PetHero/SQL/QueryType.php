87745edfa8a887bba411ab86df6fb28b3e0d3e09
elianolivera <72286803+elianolivera@users.noreply.github.com>
2022-11-10 11:21:56 -0300

    form reserva actualizzado y owner menu
    Form reserva actualizado  y owner menu con sus datos

--- PetHero/SQL/QueryType.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace SQL;
-    
-    abstract class QueryType
-    {
-        const Query = 0;
-        const StoredProcedure = 1;
-    }
-?>
--- PetHero/Views/nav-new-user.php
+++
@@ -1,8 +0,0 @@
-<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
-    <span class="navbar-text">
-        <strong>Pet Hero</strong>
-    </span>
-    <ul class="navbar-nav ml-auto">
-        
-    </ul>
-</nav>
