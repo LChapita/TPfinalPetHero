3e883f66b72a726458a2dc3287d7d61b73b1b217
LChapita <92611605+LChapita@users.noreply.github.com>
2022-11-12 16:21:45 -0300

    probando reservs

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
@@ -20,8 +20,10 @@
 	Router::Route(new Request());
 
 	require_once(VIEWS_PATH."footer.php");
-	*/
+	
 ?>
+
+<!-- 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -42,9 +44,24 @@
 		<label>
 			<h5>Add finish Date: </5>
 		</label>
+		
 		<input type="date" name="finish" required>
 		<button type="submit" name="consultar" value="consultar">Consultar</button>
 	</form>
 </body>
 
-</html>
+</html> 
+
+<!DOCTYPE html>
+ <html lang="en">
+	 <head>
+		 <meta charset="UTF-8">
+		 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		 <title>Calendario web</title>
+		</head>
+		<body>
+			
+			</body>
+			</html>
+		-->
