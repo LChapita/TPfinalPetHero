4f369a3a470d6ef2cfe15b3f84150684b84ce0bc
LChapita <92611605+LChapita@users.noreply.github.com>
2022-11-13 16:09:34 -0300

    news

--- index.php
+++ index.php
@@ -23,11 +23,6 @@
 	
 ?>
 
-<<<<<<< HEAD
-<!-- 
-
-=======
->>>>>>> 66fdbbf9cfa9375778d30b72a69990e24ea97fca
 <!DOCTYPE html>
 <html lang="en">
 
@@ -47,31 +42,9 @@
 		<label>
 			<h5>Add finish Date: </5>
 		</label>
-<<<<<<< HEAD
-		
-=======
->>>>>>> 66fdbbf9cfa9375778d30b72a69990e24ea97fca
 		<input type="date" name="finish" required>
 		<button type="submit" name="consultar" value="consultar">Consultar</button>
 	</form>
 </body>
 
-<<<<<<< HEAD
-</html> 
-
-<!DOCTYPE html>
- <html lang="en">
-	 <head>
-		 <meta charset="UTF-8">
-		 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-		 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		 <title>Calendario web</title>
-		</head>
-		<body>
-			
-			</body>
-			</html>
-		-->
-=======
 </html>
->>>>>>> 66fdbbf9cfa9375778d30b72a69990e24ea97fca
--- sumit.php
+++ sumit.php
@@ -20,19 +20,11 @@ if (isset($_FILES["photo"])) {
     move_uploaded_file($ruta_provicional, $src);
     $imagen ="fotos/" . $name;
 }
-<<<<<<< HEAD
-
-//echo "<img src=".$imagen.">";
-
-//echo "<td><img src=".VIEWS_PATH. 'keepers/photos/golden.jpg'.">" ;
-*/
-=======
 */
 //echo "<img src=".$imagen.">";
 
 //echo "<td><img src=".VIEWS_PATH. 'keepers/photos/golden.jpg'.">" ;
 
->>>>>>> 66fdbbf9cfa9375778d30b72a69990e24ea97fca
 
 error_reporting(E_ALL ^ E_NOTICE);
 $start = $_POST['start'];
@@ -48,10 +40,6 @@ $reservFinish = date("Y-m-d", strtotime("2022/11/09"));
 
 $fechasKeeper=array();
 $reservas=array();
-<<<<<<< HEAD
-
-=======
->>>>>>> 66fdbbf9cfa9375778d30b72a69990e24ea97fca
 for ($i = $start; $i <= $finish; $i = date("Y-m-d", strtotime($i . "+ 1 days"))) {
     array_push($fechasKeeper,$i);
 }
@@ -60,11 +48,7 @@ for ($i = $reservStart; $i <= $reservFinish; $i = date("Y-m-d", strtotime($i . "
 }
 
 echo "<br>";
-<<<<<<< HEAD
-foreach($fechasKeeper as $value){
-=======
 /*foreach($fechasKeeper as $value){
->>>>>>> 66fdbbf9cfa9375778d30b72a69990e24ea97fca
     echo "<br>".$value;
 }
 echo "<br>";
@@ -72,65 +56,6 @@ foreach ($reservas as $value) {
     echo "<br>" . $value;
 }
 echo "<br>";
-<<<<<<< HEAD
-
-
-/*
-$reservStart2= date("Y-m-d", strtotime("2022/11/02"));
-$reservFinish2 = date("Y-m-d", strtotime("2022/11/04"));
-$reservas2 = array();
-
-for ($i = $reservStart2; $i <= $reservFinish2; $i = date("Y-m-d",strtotime($i . "+ 1 days")))
-{
-    array_push($reservas2, $i);
-}
-
-foreach ($reservas2 as $value) {
-    echo "<br>" . $value;
-}
-echo "<br>";
-foreach($reservas2 as $value)
-{
-    array_push($reservas,$value);
-}
-*/
-sort($reservas);
-/*
-foreach ($reservas as $value) {
-    echo "<br>" . $value;
-}*/
-
-$reservasKeepers=array_diff($fechasKeeper,$reservas); ///la mejor de todas
-echo "<br>";
-
-echo "<select name='fechas'>". "fechas disponibles";
-foreach ($reservasKeepers as $value) {
-    
-    echo "<option value='" . $value."'>".$value."</option>";
-}
-echo "</select>";
-/*
-echo "<br>";
-echo "<input type='date'";
-echo "min='".print_r(array_values($reservas2))."'";
-echo "max='" . print_r(array_values($reservas2)) . "'";
-echo ">";
-*/
-/*
-$textos = array("Hola", "Chau", "Bien", "Mal");
-
-echo "Borrando la palabra 'Chau' dentro del array:<br>";
-if (($clave = array_search("Chau", $textos)) !== false) {
-    unset($textos[$clave]);
-    print_r($textos);
-}
-var_dump($textos);
-*/
-//echo "<br>".$reservStart;
-//echo $dateStart;
-//echo $dateFinish;
-
-=======
 */
 
 $reservasKeepers=array();
@@ -145,5 +70,4 @@ foreach($fechasKeeper as $valueK){
 //echo "<br>".$reservStart;
 //echo $dateStart;
 //echo $dateFinish;
->>>>>>> 66fdbbf9cfa9375778d30b72a69990e24ea97fca
 ?>
