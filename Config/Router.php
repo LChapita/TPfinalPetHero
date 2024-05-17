fd62c8d6bef72a8379378a1543c307815b59dca2
LChapita <92611605+LChapita@users.noreply.github.com>
2024-05-17 17:25:09 -0300

    Add files via upload

--- Config/Router.php
+++ Config/Router.php
@@ -1,26 +1,35 @@
-<?php 
-    namespace Config;
+<?php
 
-    use Config\Request as Request;
+namespace Config;
 
-    class Router
+use Config\Request as Request;
+
+class Router
+{
+    public static function Route(Request $request)
     {
-        public static function Route(Request $request)
-        {
-            $controllerName = $request->getcontroller() . 'Controller';
+        $controllerName = $request->getcontroller() . 'Controller';
+        $methodName = $request->getmethod();
+        $methodParameters = $request->getparameters();
 
-            $methodName = $request->getmethod();
+        // Verificar si $start está presente en los parámetros y eliminarlo si es necesario
+        if (isset($methodParameters['start'])) {
+            unset($methodParameters['start']);
+        }
 
-            $methodParameters = $request->getparameters();          
+        // Verificar si $finish está presente en los parámetros y eliminarlo si es necesario
+        if (isset($methodParameters['finish'])) {
+            unset($methodParameters['finish']);
+        }
 
-            $controllerClassName = "Controllers\\". $controllerName;            
+        $controllerClassName = "Controllers\\" . $controllerName;
 
-            $controller = new $controllerClassName;
-            
-            if(!isset($methodParameters))            
-                call_user_func(array($controller, $methodName));
-            else
-                call_user_func_array(array($controller, $methodName), $methodParameters);
+        $controller = new $controllerClassName;
+
+        if (!isset($methodParameters)) {
+            call_user_func(array($controller, $methodName));
+        } else {
+            call_user_func_array(array($controller, $methodName), $methodParameters);
         }
     }
-?>
+}
